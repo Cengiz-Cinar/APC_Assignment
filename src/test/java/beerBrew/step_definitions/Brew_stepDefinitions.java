c38969a37ff75ab06804329599509b861f719a20
CengizCinar <bandirmausak@gmail.com>
2022-09-12 14:13:47 +0200

    Task Completed

--- src/test/java/beerBrew/step_definitions/Brew_stepDefinitions.java
+++ src/test/java/beerBrew/step_definitions/Brew_stepDefinitions.java
@@ -54,7 +54,7 @@ public class Brew_stepDefinitions {
 
     @Then("Response should match with json schema")
     public void response_should_match_json_schema() {
-        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/resources/getOneBeerStyle_JSONSchema.json")));
+        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/resources/jsonSchemas/getOneBeerStyle_JSONSchema.json")));
     }
 
     @Then("status code should not be {int}")
